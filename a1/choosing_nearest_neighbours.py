00e3b4cf2ad5fdf11084b554dab709345090ecff
Connor Smith <connorjsmith101@gmail.com>
2018-01-18 18:21:16 -0500

    Used tensorflow rather than numpy

--- a1/choosing_nearest_neighbours.py
+++ a1/choosing_nearest_neighbours.py
@@ -4,15 +4,32 @@ import tensorflow as tf
 
 # Returns the responsibility vector r* for the new_point
 def get_responsibility_vector_indices(training_vectors, new_point, k):
-    new_point = new_point.reshape(1, new_point.shape[0]) # 1 x D matrix to match euclidean_distance function
+    new_point = tf.reshape(new_point, [1,-1])
     pairwise_distances = euclidean_distance(training_vectors, new_point)
-    _, indices = tf.nn.top_k(-pairwise_distances.flatten(), k, sorted=True) # k nearest points
-    return indices.eval()
+    values, indices = tf.nn.top_k(tf.reshape(-pairwise_distances, [-1]), k, sorted=True, name="responsibility_indices") # k nearest points
+    return indices
+
 
 def get_responsibility_vector(training_vectors, new_point, k):
     indices = get_responsibility_vector_indices(training_vectors, new_point, k)
-    responsibility_value = 1./k
-    r = np.zeros(training_vectors.shape[0])
-    r[indices] = responsibility_value # sets the value of all elements at a position listed in `indices` to be set to 1/k
+    responsibility_value = 1/k
+    off_value = tf.constant(0, dtype=tf.float64)
+    r_depth = training_vectors.shape[0]
+    r = tf.one_hot(indices=indices, depth=r_depth, on_value=responsibility_value, off_value=off_value, dtype=tf.float64)
+    r = tf.reduce_sum(r, axis=0)
     return r # yT . r = k-NN prediction function y^
+
+
+if __name__ == '__main__':
+    sess = tf.InteractiveSession()
+
+    t = tf.constant([[1,2,3],[4,5,6], [7,1,3], [6,0,1], [7,8,9], [3,6,8]])
+    n = tf.constant([[3,4,5]])
+    expected_indices = tf.constant([[1,0,3]])
+    k = tf.constant(3, dtype=tf.int32)
+
+    # tf.assert_equal(get_responsibility_vector_indices(t,n,k), expected_indices)
+    print(sess.run(get_responsibility_vector_indices(t,n,k)))
+
+    print(sess.run(get_responsibility_vector(t,n,k)))
 
--- a1/euclidean_distance.py
+++ a1/euclidean_distance.py
@@ -1,16 +1,21 @@
-import numpy as np
+import tensorflow as tf
+
 
 def euclidean_distance(X, Z):
-    D = X.shape[1]
-    N1 = X.shape[0]
-    N2 = Z.shape[0]
-    X_int = X.reshape(N1,1,D)
-    Z_int = Z.reshape(1,N2,D)
+    D = X.shape[-1]
+    X_int = tf.reshape(X, [-1, 1, D])
+    Z_int = tf.reshape(Z, [1, -1, D])
 
-    distance_pairs = X_int - Z_int # N1xN2xD matrix
-    squared_euclidean_distances = np.square(distance_pairs).sum(axis=2) #  N1xN2 matrix of squared pairwise distances
-    euclidean_distances = np.sqrt(squared_euclidean_distances) # element-wise squareroot
+    distance_pairs = X_int - Z_int
+    eucl_dist = tf.reduce_sum(tf.square(distance_pairs), -1, name="euclidean_distances")
+    return eucl_dist
 
-    return euclidean_distances
+if __name__ == '__main__':
+    session = tf.InteractiveSession()
+    X = tf.constant([[1,2,3], [4,5,6]])
+    Z = tf.constant([[7,8,9], [1,2,3]])
 
+    expected_result = tf.constant([[108, 0], [27, 27]])
 
+    tf.assert_equal(euclidean_distance(X,Z), expected_result)
+    print(session.run(euclidean_distance(X,Z)))
