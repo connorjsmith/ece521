57437e89925e39cd1d279d2ff0f85628f2c088e4
Connor Smith <connor.smith@mail.utoronto.ca>
2018-03-14 18:34:44 -0400

    2.2.1 working maybe?

--- a2/logistic_regression_multi.py
+++ a2/logistic_regression_multi.py
@@ -11,24 +11,34 @@ def load_notmnist_data():
         Data = Data[randIndx]/255
         Target = Target[randIndx]
         trainData, trainTarget = Data[:15000], Target[:15000]
+        t = np.zeros((trainTarget.shape[0], 10))
+        t[np.arange(trainTarget.shape[0]), trainTarget] = 1
+        trainTarget = t
         validData, validTarget = Data[15000:16000], Target[15000:16000]
+        t = np.zeros((validTarget.shape[0], 10))
+        t[np.arange(validTarget.shape[0]), validTarget] = 1
+        validTarget = t
         testData, testTarget = Data[16000:], Target[16000:]
-        return (trainData, TrainTarget, validData, validTarget, testData, testTarget)
+        t = np.zeros((testTarget.shape[0], 10))
+        t[np.arange(testTarget.shape[0]), testTarget] = 1
+        testTarget = t
+        return (trainData.reshape(trainData.shape[0], -1), trainTarget, validData.reshape(validData.shape[0], -1), validTarget, testData.reshape(testData.shape[0], -1), testTarget)
         
 
 def multiclass_not_mnist():
-    trainData, TrainTarget, validData, validTarget, testData, testTarget = load_notmnist_data()
-    with tf.Graph.as_default():
+    xTrain, yTrain, xValid, yValid, xTest, yTest = load_notmnist_data()
+    with tf.Graph().as_default():
         decay = 0.01
         B = 500
         iters = 5000
-        learning_rate = tf.constant(0.005, dtype=tf.float32, name="learning-rate")
+        learning_rate = tf.constant(0.001, dtype=tf.float32, name="learning-rate")
         
+
         num_iters_per_epoch = len(xTrain)//B # number of iterations we have to do for one epoch
         print("Num epochs = ",iters/num_iters_per_epoch)
 
         # optimized parameters
-        w = tf.Variable(tf.truncated_normal(shape=[784,1], stddev=0.5, seed=521), dtype=tf.float32, name="weight-vector")
+        w = tf.Variable(tf.truncated_normal(shape=[784,10], stddev=0.5, seed=521), dtype=tf.float32, name="weight-vector")
         b = tf.Variable(tf.zeros([1]), dtype=tf.float32, name="bias-term")
 
         # input tensors
@@ -37,16 +47,23 @@ def multiclass_not_mnist():
         xValidTensor = tf.constant(xValid, dtype=tf.float32, name="X-Validation")
         yValidTensor = tf.constant(yValid, dtype=tf.float32, name="Y-Validation")
 
+        # Create randomly shuffled batches 
+        Xslice, yslice = tf.train.slice_input_producer([xTrainTensor, yTrainTensor], num_epochs=None)
+
+        Xbatch, ybatch = tf.train.batch([Xslice, yslice], batch_size = B)
+
         # setting up batch loss function
         y_pred = tf.matmul(Xbatch, w) + b
-        # TODO: change this to be the softmax function:w
-        logLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=ybatch)) + decay * tf.nn.l2_loss(w)
+        softmaxLoss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=ybatch)) + decay * tf.nn.l2_loss(w)
 
         # accuracy function TODO these are also probably wrong
-        train_y_pred = tf.round(tf.sigmoid(tf.matmul(xTrainTensor, w) + b))
-        valid_y_pred = tf.round(tf.sigmoid(tf.matmul(xValidTensor, w) + b))
-        train_accuracy = tf.count_nonzero(tf.equal(train_y_pred, yTrainTensor)) / yTrainTensor.shape[0]
-        valid_accuracy = tf.count_nonzero(tf.equal(valid_y_pred, yValidTensor)) / yValidTensor.shape[0]
+        train_y_pred = tf.sigmoid(tf.matmul(xTrainTensor, w) + b)
+        valid_y_pred = tf.sigmoid(tf.matmul(xValidTensor, w) + b)
+        train_accuracy = tf.count_nonzero(tf.equal(tf.argmax(train_y_pred, 1), tf.argmax(yTrainTensor, 1))) / yTrainTensor.shape[0]
+        valid_accuracy = tf.count_nonzero(tf.equal(tf.argmax(valid_y_pred, 1), tf.argmax(yValidTensor, 1))) / yValidTensor.shape[0]
+
+        # optimizer function
+        optimizer = tf.train.AdamOptimizer(learning_rate).minimize(softmaxLoss)
 
         loss_amounts = []
         valid_accuracies = []
@@ -59,11 +76,11 @@ def multiclass_not_mnist():
             for i in range(iters):
                 sess.run([optimizer], feed_dict={learning_rate: 0.001})
                 if (i % num_iters_per_epoch == 0):
-                    loss_amount, train_acc, valid_acc = sess.run([train_logLoss, train_accuracy, valid_accuracy])
+                    loss_amount, train_acc, valid_acc = sess.run([softmaxLoss, train_accuracy, valid_accuracy])
                     loss_amounts.append(loss_amount)
                     valid_accuracies.append(valid_acc)
                     train_accuracies.append(train_acc)
-                    print("Epoch: {}, Loss: {}".format(i//num_iters_per_epoch, loss_amount))
+                    print("Epoch: {}, Loss: {}, trainAcc: {}".format(i//num_iters_per_epoch, loss_amount, train_acc))
             coord.request_stop()
             coord.join(threads)
             np.save("2.2.1_notmnist_loss", loss_amounts)
@@ -71,3 +88,4 @@ def multiclass_not_mnist():
             np.save("2.2.1_notmnist_t_acc", train_accuracies)
 
 
+multiclass_not_mnist()
