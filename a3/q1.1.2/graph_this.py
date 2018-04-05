f866a6722a9dea7310ad025d95c7cb856f136d23
Connor Smith <connorjsmith101@gmail.com>
2018-04-04 22:55:10 -0400

    Updated graphing code, added plots

--- a3/q1.1.2/graph_this.py
+++ a3/q1.1.2/graph_this.py
@@ -1,18 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-learning_rates = [0.1, 0.01, 0.0001, 0.005, 0.001]
+learning_rates = [0.01, 0.001, 0.005]
 
 plt.figure(figsize=(8,6), dpi=80)
 plt.title("Comparison of Loss vs. Epoch for Varied Learning Rates", fontsize=10)
 plt.subplot(1,1,1)
 plt.xlabel("# of Epochs")
 plt.ylabel("Training Loss (Cross-Entropy Loss)")
-plt.legend(title="Training Rate", loc="upper right")
-plt.grid('on', linestyle='-', linewidth=0.5)
 for r in learning_rates:
     l = np.load("1.1.2_r{}_loss.npy".format(r))
     plt.plot(l, linewidth=1.0, linestyle='-', label="Learning Rate = {}".format(r))
+plt.grid('on', linestyle='-', linewidth=0.5)
+plt.legend(title="Training Rate", loc="upper right")
 plt.savefig("1.1.2_loss_comparison.pdf", format="pdf")
 
 plt.figure(figsize=(8,6), dpi=80)
@@ -20,12 +20,12 @@ plt.title("Comparison of Training Set Accuracy vs. Epoch for Varied Learning Rat
 plt.subplot(1,1,1)
 plt.xlabel("# of Epochs")
 plt.ylabel("Training Set Accuracy (Percentage Correct)")
-plt.legend(title="Training Rate", loc="upper right")
-plt.grid('on', linestyle='-', linewidth=0.5)
 for r in learning_rates:
     l = np.load("1.1.2_r{}_train_acc.npy".format(r))
-    l * 100
+    l *= 100
     plt.plot(l, linewidth=1.0, linestyle='-', label="Learning Rate = {}".format(r))
+plt.grid('on', linestyle='-', linewidth=0.5)
+plt.legend(title="Training Rate", loc="lower right")
 plt.savefig("1.1.2_train_acc_comparison.pdf".format(r), format="pdf")
 
 plt.figure(figsize=(8,6), dpi=80)
@@ -33,12 +33,12 @@ plt.title("Comparison of Validation Set Accuracy vs. Epoch for Varied Learning R
 plt.subplot(1,1,1)
 plt.xlabel("# of Epochs")
 plt.ylabel("Validation Set Accuracy (Percentage Correct)")
-plt.legend(title="Training Rate", loc="upper right")
-plt.grid('on', linestyle='-', linewidth=0.5)
 for r in learning_rates:
     l = np.load("1.1.2_r{}_valid_acc.npy".format(r))
-    l * 100
+    l *= 100
     plt.plot(l, linewidth=1.0, linestyle='-', label="Learning Rate = {}".format(r))
+plt.grid('on', linestyle='-', linewidth=0.5)
+plt.legend(title="Training Rate", loc="lower right")
 plt.savefig("1.1.2_valid_acc_comparison.pdf".format(r), format="pdf")
 
 plt.figure(figsize=(8,6), dpi=80)
@@ -46,12 +46,13 @@ plt.title("Comparison of Test Set Accuracy vs. Epoch for Varied Learning Rates",
 plt.subplot(1,1,1)
 plt.xlabel("# of Epochs")
 plt.ylabel("Test Set Accuracy (Percentage Correct)")
-plt.legend(title="Training Rate", loc="upper right")
-plt.grid('on', linestyle='-', linewidth=0.5)
 for r in learning_rates:
     l = np.load("1.1.2_r{}_test_acc.npy".format(r))
-    l * 100
+    l *= 100
     plt.plot(l, linewidth=1.0, linestyle='-', label="Learning Rate = {}".format(r))
+    plt.plot(l, linewidth=1.0, linestyle='-', label="Learning Rate = {}".format(r))
+plt.grid('on', linestyle='-', linewidth=0.5)
+plt.legend(title="Training Rate", loc="lower right")
 plt.savefig("1.1.2_test_acc_comparison.pdf".format(r), format="pdf")
 
 
