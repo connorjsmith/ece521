459c2d9ce3f2ab2316b031786cd98bd407ac6696
Jeffrey Kirman <jeffrey.kman@gmail.com>
2018-04-06 12:09:55 -0400

    Finished part 1.2 in report

--- a3/plottingq1_2.py
+++ a3/plottingq1_2.py
@@ -15,17 +15,17 @@ fig.subplots_adjust(top=0.85, left = .15, right = .85, bottom = .15)
 ax.set_title('2 Hidden Layers of 500 Nodes')
 
 ax.set_xlabel('# of epochs')
-ax.set_ylabel('test accuracy')
+ax.set_ylabel('validation classification error')
 
 data = np.load("Q1-2-2.npy")
 
-ax.plot(np.transpose(data[0,:,:,3]))
+ax.plot(1 - np.transpose(data[0,:,:,2]))
 etas = [0.01, 0.005, 0.001, 0.0005, 0.0001];
-ax.legend(["$\eta$ = "+str(i) for i in etas], loc='lower right')
+ax.legend(["$\eta$ = "+str(i) for i in etas], loc='upper right')
 
 
 ax.axis([0, 166, 0, 1])
 
 plt.show()
 
-fig.savefig("2_hidden_layers_test.pdf")
+fig.savefig("2_hidden_layers_val_cacc.pdf")
