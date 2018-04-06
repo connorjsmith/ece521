12eb8eca2930d1b64c0a40e4807a13d4ae59ceb3
Connor Smith <connorjsmith101@gmail.com>
2018-04-06 14:21:57 -0400

    Combined images and uploaded

---
+++ a3/q1.3.2_images/1.3.2/non-dropout/100%/combine.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+image_w_h = 396
+width = image_w_h*10
+height = image_w_h*10
+new_im = Image.new('RGBA', (width, height))
+
+x_offset = 0
+for irow in range(0, 10): 
+    y_offset = 0
+    for icol in range(0, 10): 
+        img = Image.open("100_nondropout{}.png".format(irow*10+icol))
+        new_im.paste(img, (x_offset, y_offset))
+        y_offset += image_w_h
+    x_offset += image_w_h
+
+
+new_im.save('100_nondropout_combined.png')
